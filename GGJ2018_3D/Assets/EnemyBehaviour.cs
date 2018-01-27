6e01a0680b1ca713c62e7f2305aad12d02264972
John Harden <john.m.harden@gmail.com>
2018-01-27 15:56:47 -0800

    Updated enemy behaviour and added spawn limit

--- GGJ2018_3D/Assets/EnemyBehaviour.cs
+++ GGJ2018_3D/Assets/EnemyBehaviour.cs
@@ -6,12 +6,20 @@ public class EnemyBehaviour : MonoBehaviour {
 
 	public float delay = 1;
 	public GameObject enemy;
+	public float time  = 0;
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("Spawn", delay, delay);
 	}
 	
 	// Update is called once per frame
+	void Update() {
+		time++;
+		if (time > 500) {
+			CancelInvoke ();
+		}
+	}
+
 	void Spawn(){
 		Instantiate (enemy, new Vector3 (Random.Range (10, 100), 3, Random.Range(10,100)), Quaternion.identity);
 	}
--- GGJ2018_3D/Assets/HuntingBehaviour.cs
+++ GGJ2018_3D/Assets/HuntingBehaviour.cs
@@ -6,18 +6,33 @@ public class HuntingBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject player;
-	public float enemySpeed = 2;
+	public float enemySpeed = 5;
+	float timeCounter = 0;
+
+	public Vector3 jump;
+	public float jumpForce = 2.0f;
+
+	Rigidbody rb;
 
 	void Start () {
 		player = GameObject.Find ("PlayerObject");
+		rb = GetComponent<Rigidbody>();
+		jump = new Vector3(0.0f, 2.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		timeCounter++;
 		Transform target = player.transform;
-		float step = enemySpeed * Time.deltaTime;
-		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-		
+		float step = enemySpeed * (Time.deltaTime * (enemySpeed*2));
+		float distance = Vector3.Distance (transform.position, target.position);
+		Debug.Log("enemy distance: " + distance);
+		if (distance < 30) {
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		} else if(timeCounter > 100){
+			rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+			timeCounter = 0;
+			// rigidbody.AddForce (up * 5, ForceMode.Impulse);
+		}
 	}
 }
