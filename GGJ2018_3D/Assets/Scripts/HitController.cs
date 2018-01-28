a2b451900bd5025b677dde71ff061060ad7449e5
Unknown <nikolas.vulama@iugome.com>
2018-01-27 20:09:31 -0800

    hitbox and collectbble

---
+++ GGJ2018_3D/Assets/Scripts/HitController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitController : MonoBehaviour {
+
+	GameObject healthSlider;
+
+	// Use this for initialization
+	void Awake () {
+		healthSlider = GameObject.Find("HealthSlider");
+		healthSlider.GetComponent<Slider>().value = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "Enemy") {
+			PlayerStats.health -= 10;
+
+			
+			healthSlider.GetComponent<Slider>().value = PlayerStats.health;
+		}
+	}
+}
