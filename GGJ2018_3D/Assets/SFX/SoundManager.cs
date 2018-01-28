1bd1259199cdd3f5193d5b99dab08b918a9e8a6e
Michael Nguyen <mtn9@sfu.ca>
2018-01-27 17:27:11 -0800

    sound manager and sound fx

---
+++ GGJ2018_3D/Assets/SFX/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+  public AudioSource BackgroundMusic;
+  public AudioSource FXSource;
+  public static SoundManager instance = null;
+
+  void Awake()
+  {
+    if (instance == null)
+    {
+      instance = this;
+    }
+    else if (instance != this)
+    {
+      Destroy(gameObject);
+    }
+
+    DontDestroyOnLoad(gameObject);
+  }
+
+  public void PlaySingle(AudioClip clip)
+  {
+    FXSource.clip = clip;
+    FXSource.Play();
+  }
+}
