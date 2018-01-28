826121790d7e55cabe7a1a611f674ab8bc3ea987
Unknown <nikolas.vulama@iugome.com>
2018-01-28 15:40:56 -0800

    Added first screen

---
+++ GGJ2018_3D/Assets/Scripts/fullscreenTexture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fullscreenTexture : MonoBehaviour {
+
+	 public Texture backgroundTexture;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+   		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture, ScaleMode.StretchToFill);
+  	}
+}
