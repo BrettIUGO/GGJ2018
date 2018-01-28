8ac37708989ca69b2e41b09d0055427c04bb8523
Nik <nikolas.vulama@iugome.com>
2018-01-27 16:36:30 -0800

    collectable stuff

---
+++ GGJ2018_3D/Assets/Scripts/CollectableController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour {
+
+	public float collectTime = 3f;
+	public string itemType = "metal";
+	public int maxCollects = 1;
+
+	public int amount = 1;
+
+	protected float collectDuration = 0f;
+	protected int currentCollects = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onTriggerEnter() {
+		//collectStartTime = Time.time;
+	}
+
+	void OnTriggerStay () {
+		collectDuration += Time.deltaTime;
+		print(collectDuration);
+		if(collectDuration > collectTime) {
+			Collect();
+			collectDuration = 0f;
+		}
+
+	}
+
+	void OnTriggerExit(){
+		collectDuration = 0f;
+	}
+
+	void Collect() {
+		currentCollects += 1;
+		print("collected " + amount + " " + itemType);
+		PlayerStats.metal += amount;
+
+		if(currentCollects >= maxCollects) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ GGJ2018_3D/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+    public static float maxHealth = 100;
+    public static float health = 100;
+
+    public static float maxMana = 100;
+    public static float mana = 100;
+
+    public static float manaRegenRate = 20;
+
+    public static int gold = 100;
+
+    public static int metal = 0;
+
+}
